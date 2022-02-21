d71f96c13d0b237af29dda31c1b63fee62ce0a0f
chjh6107 <chjh6107@gmail.com>
2022-02-21 23:42:21 +0900

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,33 @@ function App() {
   return (
     <div>
       <h1>Super Converter test</h1>
-      <label htmlFor="minutes">Minutes</label>
-      <input value={minutes} id="minutes" placeholder="Minutes" type="number" onChange={ e => {
-          setMinutes(Number(e.target.value));
-          console.log(e.target.value);
-        }
-      } />
-      <h4>You want to convert {minutes}</h4>
-      <label htmlFor="hours">Hours</label>
-      <input value={hours} id="hours" placeholder="Hours" type="number" />
+      <div>
+        <label htmlFor="minutes">Minutes</label>
+        <input
+          value={minutes}
+          id="minutes"
+          placeholder="Minutes"
+          type="number"
+          onChange={(e) => {
+            setMinutes(Number(e.target.value));
+          }}
+        />
+      </div>
+      <div>
+        <label htmlFor="hours">Hours</label>
+        <input
+          value={Number(minutes) / 60}
+          id="hours"
+          placeholder="Hours"
+          type="number"
+          disabled
+        />
+      </div>
+      <button onClick={() => {
+        setMinutes(0);
+      }}>
+        Reset
+      </button>
     </div>
   );
 }
