a3ebdafbc1d96839f5188dc7f771fe4eb84baabb
chjh6107 <chjh6107@gmail.com>
2022-03-02 23:25:50 +0900

    .

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -14,7 +14,7 @@ const Movie = ({ id, coverImg, title, summary, genres }: MovieType) => {
       <h2>
         <Link to={"movie/" + id}>{title}</Link>
       </h2>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
         {genres.map((g: any) => (
           <li key={g}>{g}</li>
